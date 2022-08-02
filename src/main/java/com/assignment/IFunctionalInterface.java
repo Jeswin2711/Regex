a908b0dda0d8c9012e6d59bc020caeb882692239
Jeswin2711 <jeswin2711@gmail.com>
2022-08-02 21:35:23 +0530

    Added : Comments

--- src/main/java/com/assignment/IFunctionalInterface.java
+++ src/main/java/com/assignment/IFunctionalInterface.java
@@ -1,6 +1,10 @@
 package com.assignment;
 
+@FunctionalInterface
 public interface IFunctionalInterface<R>
 {
+    /*
+    Predicate PredefinedFunctionalInterface to test the String
+     */
     R test(String a);
 }
